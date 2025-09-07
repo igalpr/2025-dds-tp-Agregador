8e5544298e9bacaed1496b79c94ceb78c642a9cd
igalPR <ipistermanrozenek@frba.utn.edu.ar>
2025-09-07 19:35:18 -0300

    Se agrega configuracion de base de datos

--- src/main/java/ar/edu/utn/dds/k3003/controller/CollectionController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/CollectionController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/coleccion")
 public class CollectionController {
 	private final Fachada fachadaAgregador;
 	
