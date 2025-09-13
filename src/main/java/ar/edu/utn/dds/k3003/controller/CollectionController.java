a51d59b9e60606ae87f39682cca110635c141ac8
igalPR <igalpr2002@gmail.com>
2025-09-12 21:14:24 -0300

    Se agregaron metricas en coleccion

--- src/main/java/ar/edu/utn/dds/k3003/controller/CollectionController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/CollectionController.java
@@ -3,6 +3,8 @@ package ar.edu.utn.dds.k3003.controller;
 import ar.edu.utn.dds.k3003.facades.dtos.HechoDTO;
 import ar.edu.utn.dds.k3003.model.ErrorResponse;
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import ar.edu.utn.dds.k3003.app.Fachada;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -22,22 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class CollectionController {
 	private final Fachada fachadaAgregador;
 	private static final Logger logger = LoggerFactory.getLogger(Fachada.class);
+	private final Counter hechosRequestCounter;
+	private final Counter ColeccionNotFoundCounter;
+	private final Counter ServerErrorCounter;
 
     @Autowired
-    public CollectionController(Fachada fachadaAgregador) {
+    public CollectionController(Fachada fachadaAgregador, MeterRegistry meterRegistry) {
         this.fachadaAgregador = fachadaAgregador;
+        this.hechosRequestCounter = meterRegistry.counter("coleccion.hechos.requests");
+		this.ColeccionNotFoundCounter = meterRegistry.counter("coleccion.notfound.errors");
+		this.ServerErrorCounter = meterRegistry.counter("coleccion.server.errors");
     }
     @Timed(value = "hechos.get", description = "Time taken to return all hechos by collection")
     @GetMapping("/{nombre}/hechos")
     public ResponseEntity<?> getHechos(@PathVariable String nombre){
+    	hechosRequestCounter.increment();
     	try {
             List<HechoDTO> hechos = fachadaAgregador.hechos(nombre);
             return ResponseEntity.ok(hechos);
         } catch (NoSuchElementException e) {
+        	ColeccionNotFoundCounter.increment();
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ErrorResponse("COLLECTION_NOT_FOUND", 
                          "No se encontraron fuentes disponibles"));
         } catch (Exception e) {
+        	ServerErrorCounter.increment();
         	logger.error(e.getMessage());
             return ResponseEntity.internalServerError()
                     .body(new ErrorResponse("SERVER_ERROR", 
